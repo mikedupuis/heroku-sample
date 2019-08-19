9695f30b67f0da2ae304dd2464046e260c3d3374
Michael.Dupuis <michael.dupuis@target.com>
2019-08-19 14:10:04 -0500

    Clean up output

--- index.php
+++ index.php
@@ -7,12 +7,11 @@
 <?php
     $db_connection = pg_connect(getenv("DATABASE_URL"));
     $result = pg_query($db_connection, "select * from messages");
-    print("got result");
-    print($result);
 
     if (!pg_num_rows($result)) {
         print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
     } else {
+        var_dump(pg_fetch_all($result));
         print "Messages:\n";
         while ($row = pg_fetch_row($result)) {
             print("- $row[0]\n");
