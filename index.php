ce8553e1547d8431b85f1cd2cee71f1c41f0e8cc
Michael.Dupuis <michael.dupuis@target.com>
2019-08-19 13:36:33 -0500

    connect to the right app

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 
 
 <?php
-    $db_connection = pg_connect($(heroku config:get DATABASE_URL -a your-app));
+    $db_connection = pg_connect($(heroku config:get DATABASE_URL -a mike-sample));
     $result = pg_query($db_connection, "SELECT message, created_time FROM messages");
 ?>
