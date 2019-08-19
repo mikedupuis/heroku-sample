1d1e7a2c5005b294d9f509259d143833edd80d57
Michael.Dupuis <michael.dupuis@target.com>
2019-08-19 13:41:44 -0500

    Bug fix

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
-<?php include_once("index.html"); ?>
+<html>
+    <head>
+    </head>
 
-
+    <body>
+        <h1>Sample Heroku App</h1>
 <?php
     $db_connection = pg_connect($(heroku config:get DATABASE_URL -a mike-sample));
     $result = pg_query($db_connection, "SELECT message, created_time FROM messages");
 ?>
+    </body>
+
+</html>
