047f2305785a02973fb894f823857a5e7a5b61ca
Michael.Dupuis <michael.dupuis@target.com>
2019-08-19 13:47:39 -0500

    Use Heroku config

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <body>
         <h1>Sample Heroku App</h1>
 <?php
-    $db_connection = pg_connect($(heroku config:get DATABASE_URL -a mike-sample));
+    $db_connection = pg_connect($DATABASE_URL);
     $result = pg_query($db_connection, "SELECT message, created_time FROM messages");
 ?>
     </body>
