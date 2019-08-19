ef867a0077b43ceddade475645999f3d1b065cd7
Michael.Dupuis <michael.dupuis@target.com>
2019-08-19 14:14:28 -0500

    Clean up output

--- index.php
+++ index.php
@@ -6,15 +6,15 @@
         <h1>Sample Heroku App</h1>
 <?php
     $db_connection = pg_connect(getenv("DATABASE_URL"));
-    $result = pg_query($db_connection, "select * from messages");
+    $result = pg_query($db_connection, "select message from messages");
 
     if (!pg_num_rows($result)) {
         print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
     } else {
         var_dump(pg_fetch_all($result));
-        print "Messages:\n";
         while ($row = pg_fetch_row($result)) {
-            print("- $row[0]\n");
+            echo "Message: $row[0]";
+            echo "<br />\n";
         }
     }
 ?>
