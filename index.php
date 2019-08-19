a9cf157c636d4cbad0977a8501c4d7a723a7cf58
Michael.Dupuis <michael.dupuis@target.com>
2019-08-19 13:27:34 -0500

    Connect to database

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php include_once("index.html"); ?>
+
+
+<?php
+    $db_connection = pg_connect($(heroku config:get DATABASE_URL -a your-app));
+    $result = pg_query($db_connection, "SELECT message, created_time FROM messages");
+?>
